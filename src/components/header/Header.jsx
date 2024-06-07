f847f3dfbdbc8e7974009842c3db9090a0c6adf6
Vadim Voronin <vadimvoronin831@gmail.com>
2024-06-07 11:05:20 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import logo from './../../img/logo.svg'
+
+import './header.css'
+
+export class Header extends Component {
+    render() {
+      return (
+        <header>
+            <div className="header">
+                <div className="contin_logo">
+                    <a href="#">
+                    <img src={logo} alt="" />
+                    <span>eatly</span>
+                    </a>
+                </div>
+                <nav className='header_menu'>
+                    <ul>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Contacts</a></li>
+                    </ul>
+                </nav>
+                <div className="header_form">
+                    <button className='login'>Login</button>
+                    <button className='sign'>Sign up</button>
+                </div>
+            </div>
+        </header>
+      )
+    }
+  }
+  
+  export default Header
